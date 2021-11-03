8366d9339a53ef863b0cf888cfb1c08801d5a524
hakancenkalinoglu <79363052+hakancenkalinoglu@users.noreply.github.com>
2021-11-03 03:01:44 +0300

    Added some js and html codes

--- myScript.js
+++ myScript.js
@@ -2,3 +2,8 @@ function myFunction(){
     document.getElementById("demo").innerHTML="js run";
 
 }
+
+function darkMode(){
+    var element = document.sty
+    element.classList.toggle("dark-mode");
+}
