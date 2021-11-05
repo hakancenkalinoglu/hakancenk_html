1b94f532b9ffb9d65a20c16fbd8fed9bdb659a84
hakancenkalinoglu <79363052+hakancenkalinoglu@users.noreply.github.com>
2021-11-05 15:06:02 +0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -3,7 +3,7 @@ function myFunction(){
 
 }
 
-function darkMode(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
 }
